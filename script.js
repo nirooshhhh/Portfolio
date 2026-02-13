53ffe6c007f6af2b28af6935f068838a898f26a8
nirooshhhh <nirosh2006@gmail.com>
2026-02-13 22:18:56 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -5,3 +6,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// Typing effect
+const text = ["Frontend Developer", "React Developer", "UI Enthusiast"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+  if(count === text.length){
+    count = 0;
+  }
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 100);
+})();
